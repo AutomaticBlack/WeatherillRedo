99c486b36809dff4e835b8426d8759376b4dbafb
Michael Huang <changlin.h97@gmail.com>
2015-06-03 12:56:10 -0400

    YAS

--- Start.java
+++ Start.java
@@ -9,11 +9,15 @@ public class Start
 	Scanner keyboard = new Scanner (System.in);
 	public void startGame()
 	{
-		System.out.println("Hi, enter your name");
-
-
+		System.out.println("HELLO, WELCOME TO THE WORLD OF WEATHERILL");
+		System.out.println("Oh, you don't look too well, let me introduce myself!");
+		System.out.println("I am Jakobe, and I was walking to the discotech when I found you passed out in the ditch in the road...");
+		System.out.println("Anyways, you had about 4 dildos stuck up your ass, but... Hey! Don't look at me like that! They weren't even b-");
+		System.out.println("Ahem- Excuse me, what's your name");
+		System.out.print("(Type in your name here)  :");
 		String name = keyboard.nextLine();
 		Stats guy = new Stats(name);
+		System.out.println("Ah, so your name is "+name+"...?");
 	}
 
 
